dd0f80c979d04b5504610a0227a8d85846c3d1cc
jpo424 <jpo424@gmail.com>
2017-09-21 17:23:27 -0400

    architecture change

--- YChartsApi/YChartsApi/YChartsException.cs
+++ YChartsApi/YChartsApi/YChartsException.cs
@@ -2,8 +2,8 @@
 
 namespace YCharts.Api
 {
-    /// <summary> The base class of all YCharts Api exceptions</summary>
-    public sealed class ApiException : Exception
+    /// <summary> The base class of all YCharts API exceptions</summary>
+    public sealed class YChartsException : Exception
     {
         /// <summary> Corresponds to a request HTTP status code</summary>
         public int StatusCode;
@@ -11,7 +11,7 @@ namespace YCharts.Api
         /// <summary> Constructs an ApiException with both a status code and reason</summary>
         /// <param name="message"> Reason for exception</param>
         /// <param name="statusCode">HTTP status code of exception</param>
-        public ApiException(string message, int statusCode) : base(message)
+        internal YChartsException(string message, int statusCode) : base(message)
         {
             this.StatusCode = statusCode;
         }
@@ -19,7 +19,7 @@ namespace YCharts.Api
         /// Constructs an ApiException with a reason
         /// </summary>
         /// <param name="message">Reason for exception</param>
-        public ApiException(string message) : base(message) { }
+        internal YChartsException(string message) : base(message){}
 
     }
 }
